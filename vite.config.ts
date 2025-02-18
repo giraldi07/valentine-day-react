fd536ba5a23bfdc904ae19b573a75c4abd121cf2
giraldi07 <giraldipramay@gmail.com>
2025-02-18 11:49:59 +0700

    update efek suara gallery loveletter games dan todolist

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist', // Folder output build
+    assetsDir: 'assets', // Folder untuk aset (gambar, font, dll.)
+    emptyOutDir: true, // Bersihkan folder dist sebelum build
   },
 });
